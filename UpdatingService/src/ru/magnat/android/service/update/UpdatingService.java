ff7154418554a4fdb5eaa482901a106b0bcf653a
petr_bu <bukhal.ps@gmail.com>
2014-04-23 17:59:39 +0400

    1

--- UpdatingService/src/ru/magnat/android/service/update/UpdatingService.java
+++ UpdatingService/src/ru/magnat/android/service/update/UpdatingService.java
@@ -1,14 +1,19 @@
 package ru.magnat.android.service.update;
 
+import ru.magnat.android.service.update.aidl.IUpdatingService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
 public class UpdatingService extends Service {
 
+	private IUpdatingService.Stub mBinder = new IUpdatingService.Stub() {
+		
+	};
+	
 	@Override
 	public IBinder onBind(Intent arg0) {
-		return null;
+		return mBinder.asBinder();
 	}
 
 }
